8885ac0e7b5b5b5d704c9537917d654f6c3b0c47
mecnieri <abzogio@yahoo.com>
2019-04-16 17:31:30 +0400

    web sockets

---
+++ index.js
@@ -0,0 +1,31 @@
+let express = require("express");
+let socket = require("socket.io");
+
+// App setup
+let app = express();
+let server = app.listen(1024, function() {
+  console.log("listening to requests on port 1024");
+});
+
+// Static files
+app.use(express.static("public"));
+
+// Socket setup
+let io = socket(server);
+
+io.on("connection", function(socket) {
+  console.log("made socket connection with id of ", socket.id);
+
+  socket.on("chat", function(data) {
+    io.sockets.emit("chat", data);
+  });
+
+  socket.on("typing", function(data) {
+    socket.broadcast.emit("typing", data);
+  });
+
+  socket.on("browseridanServerze", function(params) {
+    socket.broadcast.emit("serveridan-browserebze", params);
+    // io.sockets.emit("serveridan-browserebze", params);
+  });
+});
---
+++ public/chat.js
@@ -0,0 +1,46 @@
+//  Make Connection
+
+let socket = io.connect("http://localhost:1024");
+
+// Query DOM
+let message = document.getElementById("message"),
+  handle = document.getElementById("handle"),
+  btn = document.getElementById("send"),
+  output = document.getElementById("output"),
+  feedback = document.getElementById("feedback");
+
+// Emit event
+
+btn.addEventListener("click", function() {
+  socket.emit("chat", {
+    handle: handle.value,
+    message: message.value
+  });
+  message.value = "";
+});
+
+socket.on("chat", function(data) {
+  output.innerHTML +=
+    "<p><strong>" + data.handle + ":</strong>" + data.message + "</p>";
+  feedback.innerHTML = " ";
+});
+
+message.addEventListener("keypress", function() {
+  socket.emit("typing", handle.value);
+});
+
+socket.on("typing", function(data) {
+  feedback.innerHTML = "<p><em>" + data + " is typing a message... </em></p>";
+});
+
+window.addEventListener("click", function() {
+  socket.emit("browseridanServerze", {
+    sender: handle.value,
+    X: event.clientX,
+    Y: event.clientY
+  });
+});
+
+socket.on("serveridan-browserebze", function(params) {
+  console.log(params);
+});
